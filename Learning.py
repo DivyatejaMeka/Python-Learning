49931340cfb85ed6e70c92569befa7235cf3178b
DivyatejaMeka <meka.divyateja@gmail.com>
2024-10-25 12:57:36 +0530

    Hello world and Data Types

---
+++ Learning.py
@@ -0,0 +1,14 @@
+#Hello World
+print("Hello World")
+print('Hello world!')
+print('''Hello world!!''')
+
+#Data Types
+name = "DT" #String
+height = 5.8 #Float
+age = 20 #Integer
+Student = True #Boolean
+print ("name=", name)
+print("height=", height)
+print("age=", age)
+print("Student?", Student)
