140968ff9622052fa51cd1eb0c3cd6659c3dae90
DivyatejaMeka <meka.divyateja@gmail.com>
2024-10-28 11:04:25 +0530

    Hello World and DataTypes

--- Learning.py
+++ Learning.py
@@ -8,7 +8,19 @@ name = "DT" #String
 height = 5.8 #Float
 age = 20 #Integer
 Student = True #Boolean
+x = 1j #Complex
 print ("name=", name)
 print("height=", height)
 print("age=", age)
 print("Student?", Student)
+print(x, type(x))
+
+#Input from users
+name = str(input("Enter Your name:")) #Input in form of string
+age = int(input("Enter your age:")) #Input in form of integer
+salary = float(input("Expected salary:")) #Input in form of float
+print(name)
+print(age)
+print(salary)
+
+
